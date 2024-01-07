58ba20f426e75def06f9c549732d2046044215a6
StillImmortal <immortallatromi22@gmail.com>
2024-01-07 09:53:15 +0700

    new exp branch without organizaions

--- src/app/(dashboard)/[orgSlug]/(member)/settings/(default)/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function OrganizationDefaultSettingsPage() {
-  return <div>OrganizationDefaultSettingPage</div>
-}
-
-export default OrganizationDefaultSettingsPage
--- src/app/(dashboard)/[orgSlug]/(member)/tables/[tableId]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function TeamTable() {
-  return <div>TeamTable</div>
-}
-
-export default TeamTable
